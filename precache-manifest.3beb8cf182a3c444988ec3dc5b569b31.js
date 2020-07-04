42799d457d2bdd98eb3068c6e8bbe1c3fc55081a
roopal59 <66762708+roopal59@users.noreply.github.com>
2020-07-04 17:51:55 -0400

    Updates

---
+++ precache-manifest.3beb8cf182a3c444988ec3dc5b569b31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3e9f36cf2b2eb53d397f4db7a94bec9",
+    "url": "/React-todolist/index.html"
+  },
+  {
+    "revision": "4c1692e800bdfa974e68",
+    "url": "/React-todolist/static/css/main.eb753631.chunk.css"
+  },
+  {
+    "revision": "c77cfb6398381d82cd38",
+    "url": "/React-todolist/static/js/2.9d39a40e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-todolist/static/js/2.9d39a40e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c1692e800bdfa974e68",
+    "url": "/React-todolist/static/js/main.eea6ef02.chunk.js"
+  },
+  {
+    "revision": "9bdfa64be2bc94bf3a5a",
+    "url": "/React-todolist/static/js/runtime-main.89df498e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/React-todolist/static/media/logo.5d5d9eef.svg"
+  }
+]);
